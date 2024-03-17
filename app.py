81a3260d4e6f67fb8064d1364b5361fd9f193b74
Siraj Shaikh <82135509+imsiraj@users.noreply.github.com>
2024-03-17 11:43:02 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,51 @@
+from flask import Flask, request, render_template
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+import nltk
+from string import punctuation
+import re
+from nltk.corpus import stopwords
+
+nltk.download('stopwords')
+
+# import nbimporter
+# from nbimporter import NotebookLoader
+# from cellphone_accessories_reviews import sentiment_analysis
+
+
+
+set(stopwords.words('english'))
+
+app = Flask(__name__)
+
+@app.route('/')
+def my_form():
+    return render_template('form.html')
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    stop_words = stopwords.words('english')
+    
+    #convert to lowercase
+    text1 = request.form['text1'].lower()
+    
+    # text_final = ''.join(c for c in text1 if not c.isdigit())
+    text_final = ''.join(c for c in text1 if not c.isdigit())
+    
+    #remove punctuations
+    #text3 = ''.join(c for c in text2 if c not in punctuation)
+        
+    #remove stopwords    
+    # processed_doc1 = ' '.join([word for word in text_final.split() if word not in stop_words])
+    processed_doc1 = ' '.join([word for word in text_final.split() if word not in stop_words])
+    sa = SentimentIntensityAnalyzer()
+    dd = sa.polarity_scores(text=processed_doc1)
+    compound = round((1 + dd['compound'])/2, 2)
+    # sentiment=sentiment_analysis(processed_doc1)
+
+    # return render_template('form.html', final=compound, text1=text_final)
+    return render_template('form.html', final=compound, text1=text_final,text2=dd['pos'],text5=dd['neg'],text4=compound,text3=dd['neu'])
+
+if __name__ == "__main__":
+    app.run(debug=True)
